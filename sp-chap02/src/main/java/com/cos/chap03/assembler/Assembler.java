e66703e6e4728af80000a4eee7b70d61bcdb77a7
wonka <rainbow4lade@gmail.com>
2022-10-13 13:08:25 +0900

    uploading-1

--- sp-chap02/src/main/java/com/cos/chap03/assembler/Assembler.java
+++ sp-chap02/src/main/java/com/cos/chap03/assembler/Assembler.java
@@ -12,5 +12,18 @@ public class Assembler {
 	
 	public Assembler() {
 		memberDao = new MemberDao();
+		regSvc = new MemberRegisterService(memberDao);
+		pwdSvc = new ChangePasswordService();
+		pwdSvc.setMemberDao(memberDao);		
+	}
+	public MemberDao getMemberDao() {
+	    return memberDao;
+	}
+	
+	public MemberRegisterService getMemberRegisterService() {
+	    return regSvc;
+	}
+	public ChangePasswordService getChangePasswordService() {
+	    return pwdSvc;
 	}
 }
