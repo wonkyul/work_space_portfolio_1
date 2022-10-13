9321a15b689781632379b077a51d0da0a2301a95
wonka <rainbow4lade@gmail.com>
2022-10-13 13:34:37 +0900

    add jpa-blog sample

---
+++ sp-chap02-1/src/main/java/com/cos/security1/SpChap021Application.java
@@ -0,0 +1,13 @@
+package com.cos.security1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpChap021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpChap021Application.class, args);
+	}
+
+}
